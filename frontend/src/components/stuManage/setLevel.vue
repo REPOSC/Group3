afa95a342838a039528f4a30f0454353e81bb8be
jiazi <2597892733@qq.com>
2018-08-14 22:06:58 +0800

    Complete the interface of setting up user level. Ref #16

--- frontend/src/components/stuManage/setLevel.vue
+++ frontend/src/components/stuManage/setLevel.vue
@@ -6,26 +6,23 @@
     <div class = "loginFrame">
       <form ref = "AccountForm"  rules = "rules" label-position = "left" label-width = "0px" class = "demo-ruleForm login-container">
 
-        <div style="height:30px ; float: left">新会员用户名</div>
+        <div style="height:30px ; float: left">会员账号</div>
         <input type = "text"  style="width:100px ;height:25px" auto-complete = "off" placeholder = "请输入您的账号">{{username}}
-        <br/><br/>
-        <div style="height:30px ; float: left">登陆密码</div>
-        <input type = "password" style="width:100px ;height:25px" auto-complete = "off" placeholder = "请输入密码">{{password}}
         <br/><br/>
 
         <div id = 'app' style="height:30px ; float: left" >等级选择</div>&nbsp
-          <select v-model="value" placeholder="请选择">
-    <option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </option>
-  </select>
+        <select v-model="value" placeholder="请选择">
+          <option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </option>
+        </select>
 
         <br/><br/><br/><br/>
         <form-item style = "width:100%;">
-          <el-button type = "primary" style = "width:100%;"  @click="submit" >确认注册</el-button>
+          <button type = "primary" style = "width:100%;"  @click="submit" >确认修改</button>
         </form-item>
       </form>
     </div>
@@ -74,7 +71,7 @@
           value: '选项12',
           label: '等级12'
         }
-    ],
+        ],
         value: ''
       }
     }
