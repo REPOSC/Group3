2f3b6eda139abc9edb39668a5304cda54c5c3ae4
Donald.Z <chinasdzyz@163.com>
2018-08-17 00:11:43 +0800

    Backend exchange data with client and realize login function. Ref #30

--- frontend2/src/pages/login/index.vue
+++ frontend2/src/pages/login/index.vue
@@ -1,28 +1,51 @@
 <template>
-  <div class="login">
-    <form class="form-control">
-      <div class="card">
-      <p>用户名:</p>
-      <input type='text' class="input"/>
-      <p>{{ username }}</p>
+  <div class='login'>
+    <form class='form-control'>
+      <div class='card'>
+        <p>账号:</p>
+        <input type='text' class='input' v-model='username' focus=true />
       </div>
-      <div class="card">
-      <p>密码:</p>
-      <input type='password' class="input"/>
-      <p>{{ password }}</p>
+      <div class='card'>
+        <p>密码:</p>
+        <input type='password' class='input' v-model='password'/>
       </div>
-      <button @click="tobookshelf">登录</button>
+      <button @click='handlelogin'>登录</button>
     </form>
   </div>
 </template>
 
 <script>
-
+import qs from 'qs'
 export default {
   data() {
     return {
-      username: '',
-      password: ''
+      username: '10000017',
+      password: 'bwCHikKhC5BjPit'
+    }
+  },
+  methods: {
+    test: function() {
+      console.log(this.username)
+      console.log(this.password)
+    },
+    handlelogin: function() {
+      let Fly= require('../../../node_modules/flyio/dist/npm/wx.js')
+      let fly= new Fly
+      let save = this
+      fly.post('http://192.168.55.33:8082/auth_student',qs.stringify({
+        id: save.username,
+        password: save.password
+      })).then(function(response) {
+        if (response.data.status === 'error') {
+          wx.showModal({
+            content: '账号或密码错误，请重新登录',
+          })
+        } else {
+          wx.redirectTo({
+            url: '../level/main'
+          })
+        }
+      })
     }
   }
 }
--- server/server/views.py
+++ server/server/views.py
@@ -5,20 +5,30 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 def login_manager(request):
-  return render(request, "login_manager.html")
-
+  return render(request, 'login_manager.html')
 
 def auth_manager(request):
   number = request.POST.get('id', '')
   user_pwd = request.POST.get('password', '')
   user = authenticate(username=number, password=user_pwd, is_manager=True)
   if user is None:
-    return JsonResponse({"status": "error"})
+    return JsonResponse({'status': 'error'})
   else:
     login(request, user)
-    return JsonResponse({"status": "right"})
+    return JsonResponse({'status': 'right'})
 
+def auth_student(request):
+  number = request.POST.get('id','')
+  user_pwd = request.POST.get('password','')
+  user = authenticate(username=number, password=user_pwd, is_manager=False)
+  if user is None:
+    return JsonResponse({'status':'error'})
+  else:
+    login(request,user)
+    return JsonResponse({'status':'right'})
 
 @login_required
 def main(request):
-  return render(request,"index.html")
+  return render(request,'index.html')
+
+
