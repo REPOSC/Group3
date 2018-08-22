e5938da28293584d8a8d0825f295476b35f46936
陈芳蕾 <chenfanglei1998@qq.com>
2018-08-23 02:09:26 +0800

    Add the test for level.Ref #60

---
+++ frontend/test/unit/specs/Login.spec.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Login from '@/components/Login.vue'
+
+describe('Login', () => {
+
+  it('测试data是函数', () => {
+    expect(typeof Login.data).toBe('function')
+  })
+
+  it('测试data原始数据', () => {
+    const defaultData = Login.data()
+    expect(defaultData.account.username).toBe('')
+    expect(defaultData.account.pwd).toBe('')
+  })
+})
