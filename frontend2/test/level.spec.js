473bc06d0176645b4b631d11f2e8a61b5d679f62
陈芳蕾 <chenfanglei1998@qq.com>
2018-08-24 21:48:14 +0800

    Add logic of game1. Ref #49

--- frontend2/test/level.spec.js
+++ frontend2/test/level.spec.js
@@ -13,12 +13,6 @@ describe('测试等级界面', () => {
     expect(wrapper.contains('button')).toBe(false);
   });
 
-  it('测试样式', () => {
-    expect(wrapper.is('.true')).toBe(true);
-    expect(wrapper.is('.false')).toBe(true);
-    expect(wrapper.is('.levelbtn')).toBe(true);
-  })
-
   it('测试div的显示', () => {
     expect(wrapper.contains('div')).toBe(true);
   });
