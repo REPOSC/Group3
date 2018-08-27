dad1dc78dc17d66d1416641c0b7e13e6b46cdf5f
陈芳蕾 <chenfanglei1998@qq.com>
2018-08-27 23:34:01 +0800

    Add system message page and its function. Ref #111

---
+++ frontend2/src/components/sys_message.vue
@@ -0,0 +1,95 @@
+<template>
+  <div :class="isread">
+    <div class="title">
+      <span class="time">{{ send_time }}</span>
+      <span class="expand" @click="$emit('more')">{{ expand_info }}</span>
+    </div>
+    <div class="body">
+      <p :class="content_status">{{ content }}</p>
+    </div>
+    <div class="function" v-if="content_status === 'true'">
+      <button class="mark" @click="$emit('mark_notread')">{{ isread_info }}</button>
+      <button class="close" @click="$emit('delete_mes')">删除</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'send_time',
+    'content',
+    'content_status',
+    'expand_info',
+    'isread_info',
+    'isread'
+  ]
+}
+</script>
+
+<style>
+.notread {
+  color: #ffb100;
+  border: 5px solid #ffb100;
+  margin: 10px auto;
+  border-radius: 10px;
+  padding: 5px 19px;
+  background-color: antiquewhite;
+}
+.haveread {
+  color: #53cce9;
+  border: 5px solid #53cce9;
+  margin: 10px auto;
+  border-radius: 10px;
+  padding: 5px 19px;
+  background-color: antiquewhite;
+}
+.title {
+  height: 30px;
+}
+.time {
+  font-size: 18px;
+}
+.function {
+  font-size: 16px;
+  color: white;
+  display: flex;
+  margin: 10px auto;
+}
+.close {
+  font-size: 16px;
+  width: 40%;
+  height: 30px;
+  line-height: 30px;
+  background-color: #f53076;
+}
+.mark {
+  font-size: 16px;
+  width: 40%;
+  height: 30px;
+  line-height: 30px;
+  background-color: #53cce9;
+}
+.false {
+  text-align: justify;
+  font-size: 18px;
+  text-overflow: -o-ellipsis-lastline;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  align-content: center;
+  color: #222
+}
+.true {
+  text-align: justify;
+  font-size: 18px;
+  color: #222
+}
+.expand {
+  position: absolute;
+  right: 41px;
+  font-size: 18px;
+}
+</style>
---
+++ frontend2/src/pages/system_message/index.vue
@@ -0,0 +1,77 @@
+<template>
+<div class="all-message">
+  <div class="messsage-card" v-for="n in messages.length">
+    <sys_message v-bind="messages[n-1]" @more="more(messages[n-1])" @delete_mes="delete_mes(n-1)" @mark_notread="mark(messages[n-1])"></sys_message>
+  </div>
+</div>
+</template>
+
+<script>
+import sys_message from '@/components/sys_message'
+export default {
+  components: {
+    sys_message
+  },
+  data() {
+    return {
+      messages: [
+        {
+          send_time: '2018年8月27日',
+          content: '啦啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊',
+          content_status: 'false',
+          expand_info: '展开更多',
+          isread_info: '标记为未读',
+          isread: 'notread'
+        },
+        {
+          send_time: '2018年8月29日',
+          content: '啦啦啦啦啦啦啦啦啦啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊',
+          content_status: 'false',
+          expand_info: '展开更多',
+          isread_info: '标记为未读',
+          isread: 'notread'
+        }
+      ]
+    }
+  },
+  methods: {
+    more(message) {
+      if (message.content_status === 'false') {
+        message.content_status = 'true'
+        message.expand_info = '收起'
+        message.isread = 'haveread'
+        message.isread_info = '标记为未读'
+      } else {
+        message.content_status = 'false'
+        message.expand_info = '展开更多'
+      }
+    },
+    delete_mes(index) {
+      this.messages.splice(index, 1)
+    },
+    mark(message) {
+      if (message.isread_info === '标记为未读') {
+        message.isread_info = '标记为已读'
+        message.isread = 'notread'
+      } else {
+        message.isread_info = '标记为未读'
+        message.isread = 'haveread'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+page {
+  background-size: 100% 100%;
+  background-image: url('https://daisy-donald.cn/image/back.jpg');
+}
+.all-message {
+  margin: 10px 20px;
+}
+.message-card {
+  width: 100px;
+  height: 100px;
+}
+</style>
